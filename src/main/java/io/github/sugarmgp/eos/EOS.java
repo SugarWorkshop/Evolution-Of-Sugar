cf3770c4c41fc132157d288d2b1964454947f0f0
SugarMGP <2350745751@qq.com>
2023-01-30 11:11:49 +0800

    修复version和onSetup

--- src/main/java/io/github/sugarmgp/eos/EOS.java
+++ src/main/java/io/github/sugarmgp/eos/EOS.java
@@ -4,10 +4,15 @@ import io.github.sugarmgp.eos.handler.BlockHandler;
 import io.github.sugarmgp.eos.handler.ConfigHandler;
 import io.github.sugarmgp.eos.handler.EntityHandler;
 import io.github.sugarmgp.eos.handler.ItemHandler;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.ReportedException;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,13 +24,23 @@ public class EOS {
     private static final Logger LOGGER = LogManager.getLogger("Evolution Of Sugar");
 
     public EOS() {
-        ItemHandler.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        BlockHandler.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        EntityHandler.ENTITY_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ItemHandler.ITEMS.register(bus);
+        BlockHandler.BLOCKS.register(bus);
+        EntityHandler.ENTITY_TYPES.register(bus);
+        bus.addListener(this::onSetup);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.commonConfig);
     }
 
     public static Logger getLogger() {
         return LOGGER;
+    }
+
+    public void onSetup(FMLCommonSetupEvent event) {
+        if (ModList.get().isLoaded("torcherino") || ModList.get().isLoaded("projecte")) {
+            CrashReport report = CrashReport.makeCrashReport(new IllegalAccessError(),
+                    "You have ENRAGED the FOREST BAT because some mods are loaded");
+            throw new ReportedException(report);
+        }
     }
 }
--- src/main/java/io/github/sugarmgp/eos/handler/EventHandler.java
+++ src/main/java/io/github/sugarmgp/eos/handler/EventHandler.java
@@ -1,15 +1,11 @@
 package io.github.sugarmgp.eos.handler;
 
-import net.minecraft.crash.CrashReport;
-import net.minecraft.crash.ReportedException;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber
 public class EventHandler {
@@ -21,15 +17,6 @@ public class EventHandler {
             String message = "eos.welcome";
             TranslationTextComponent text = new TranslationTextComponent(message, player.getDisplayName());
             player.sendStatusMessage(text, false);
-        }
-    }
-
-    @SubscribeEvent
-    public void onSetup(FMLCommonSetupEvent event) {
-        if (ModList.get().isLoaded("torcherino") || ModList.get().isLoaded("projecte")) {
-            CrashReport report = CrashReport.makeCrashReport(new IllegalAccessError(),
-                    "You have ENRAGED the FOREST BAT because some mods are loaded");
-            throw new ReportedException(report);
         }
     }
 }
